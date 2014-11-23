df044c9c76ed4c1144e658f828f7927cdce6b3da
garnaga <garnaga@ymail.com>
2014-11-23 22:41:07 +0200

    git remote add origin https://github.com/garnaga/untitled1.git git push -u origin master

--- lesson 6/Lesson 6 HW.js
+++ lesson 6/Lesson 6 HW.js
@@ -1,17 +1,13 @@
 jQuery( document ).ready(function( $ ) {
     $("#list").click(function (event){
+        if(event.target.tagName === "BUTTON") {
+            event.target.parentNode.remove();
+        }
         if(event.target.tagName === "LI") {
             $(event.target).toggleClass("selected");
         }
     });
-    $("#list input").click(function(event) {
-        event.stopPropagation();
-    });
-    $("#list button").click(function(event) {
-        event.stopPropagation();
-        event.target.parentNode.remove();
-    });
-    $("#deleteSelected").click(function(event) {
-        $("#list .selected").remove();
+    $("#deleteSelected").click(function() {
+        $('#list .selected').remove();
     });
 });
