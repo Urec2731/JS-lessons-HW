9cbec495a351aefca5a2fea4cc27574eb6482fc1
garnaga <garnaga@ymail.com>
2014-12-06 16:19:11 +0200

    git remote add origin https://github.com/garnaga/untitled1.git git push -u origin trash

--- lesson8/Lesson8.js
+++ lesson8/Lesson8.js
@@ -1,30 +1,82 @@
 jQuery(function( $ ) {
-    var friendList = ["Колян" ,"Петрович" ,"Марина" ,"Макс" ,"Игорь"],
-        list = $("[data-id='list']"),
-        itemTemplate = list.find('[data-class="item"].hidden');
+    function ActiveList (ops) {
+        var list = ops.modelArray,
+            container = ops.listElement,
+            itemTemplate = ops.itemTemplate;
 
-    function addItem() {
-        return itemTemplate.clone(true).appendTo(list).removeClass('hidden');
+        function addOne() {
+            return itemTemplate.clone(true).appendTo(container).removeClass('hidden');
+        };
+        function createListHTML () {
+            container.empty();
+            list.map(function(element){
+                addOne().find('input').val(element);
+            });
+        };
+
+        this.add = function ( i, el ) {
+            list[i] = el;
+        };
+        this.remove = function (el) {
+            for (var i = 0; i < list.length; i++){
+                if (list[i] === el) {
+                    list.splice(i, 1);
+                    createListHTML();
+                    return;
+                };
+            };
+        };
+        this.removeSome = function (els) {
+            els.map(function(e, el) {
+                for (var i = 0; i < list.length; i++) {
+                    if (list[i] === el) {
+                        list.splice(i, 1);
+                        return;
+                    };
+                };
+            });
+            createListHTML();
+        };
+        this.addEmptyItem = addOne;
+        this.refreshList = createListHTML;
+        createListHTML();
     };
 
-    friendList.map(function(element){
-       addItem().find('input').val(element);
-    });
 
-    list.click(function (event){
-        var target = $(event.target);
-        if (target.is('[data-class="deleteItem"]')) {
-            target.closest('[data-class="item"]').remove();
-            return;
-        }
-        if (target.is('[data-class="item"]')) {
-            target.toggleClass("selected");
-        }
-    });
+    var friendList = $('[data-id="list"]')
+        .click(function (event){
+            var target = $(event.target);
+            if (target.is('[data-class="deleteItem"]')) {
+                friends.remove( target.closest('[data-class="item"]').find('input').val() );
+                return;
+            }
+            if (target.is('[data-class="item"]')) {
+                target.toggleClass("selected");
+            }
+        });
+    friendList.on('focusout', function() {
+            var target = $(event.target);
+            if ( target.is('input') ) {
+                var i = friendList.find('input').index(target);
+                friends.add(i, target.val());
+            }
+        });
 
     $("[data-id='deleteSelected']").click(function() {
-        $('[data-class="item"].selected').remove();
+        var v = $('[data-class="item"].selected').find('input').map(function(i, el) {
+            return this.value;
+        });
+            friends.removeSome( v );
+
     });
 
-    $("[data-id='addItem']").click(addItem);
+    var friends = new ActiveList({
+        modelArray: ["Колян" ,"Петрович" ,"Марина" ,"Макс" ,"Игорь"],
+        listElement:friendList,
+        itemTemplate:$('[data-id="list"] [data-class="item"].hidden')
+    });
+
+    $("[data-id='addItem']").click(friends.addEmptyItem);
+
+
 });
