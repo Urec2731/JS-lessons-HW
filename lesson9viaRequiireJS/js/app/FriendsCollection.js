2579639fbbfe9e43e4627aede6d6a3e559c4ed68
garnaga <garnaga@ymail.com>
2015-01-07 13:29:59 +0200

    New changelist

--- lesson9viaRequiireJS/js/app/FriendsCollection.js
+++ lesson9viaRequiireJS/js/app/FriendsCollection.js
@@ -1,15 +1,18 @@
 define(["../lib/jquery", "../lib/underscore", "../lib/backbone", './FriendModel'],
     function($, _, Backbone, Friend) {
        var Friends = Backbone.Collection.extend({
-           model: Friend
+           model: Friend,
+           getFriendsFromServer: function (url){
+               var collection = this;
+               $.ajax({
+                   url: url,
+                   success:  function (data, status) {
+                       console.log(status);
+                       collection.add(data);
+                   }
+               });
+           }
        }) ;
-        $.ajax({
-            url: modelurl,
-            success:  function (data, status) {
-                                             console.log(status);
-                    collection = new Friends(data);
-                }
-            });
-
+        return new Friends();
     }
 );
