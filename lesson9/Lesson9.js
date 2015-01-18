c274d0f9342e534ae01f340bf8db1cfed215494f
garnaga <garnaga@ymail.com>
2015-01-18 17:44:34 +0200

    New changelist

--- lesson9/Lesson9.js
+++ lesson9/Lesson9.js
@@ -1,166 +1,90 @@
-(function() {
+//(function() {
+    var myFriendsFromServer = [
+        {name:"Колян"},
+        {name:"Петрович"},
+        {name:"Марина"},
+        {name:"Макс"},
+        {name:"Игорь"}
+    ];
     var Friend = Backbone.Model.extend({
         defaults: {
-            name: "",
-            age: 0
+            name: ""
         }
     });
     var FriendList = Backbone.Collection.extend({
         model: Friend
     });
-    var FriendNamesView = Backbone.View.extend({
+    var friendTemplate = $('#list [data-item]').detach();
+    var FriendView = Backbone.View.extend({
         initialize: function () {
             var view = this;
-            view.collection.map(function (model) {
-                view.addItem(model);
+            view.setElement( friendTemplate.clone() );
+            view.$name = view.$( '[data-nameInput]' );
+            view.$name.val( view.model.get('name') );
+            view.$delete = view.$( '[data-deleteItem]' );
+            view.listenTo(view.model, 'change:name', view.refreshName);
+            view.listenTo(view.model, 'destroy remove', view.removeItem);
+            view.$el.click(function(){
+                view.$el.toggleClass("selected");
             });
-            view.listenTo(view.collection, 'change:name', view.refreshName);
-            view.listenTo(view.collection, 'remove', view.removeItem);
+            view.$name.click(function(event) {
+                event.stopPropagation();
+            });
+            view.$name.blur( function () {
+                    if (view.model.get('name') !== view.$name.val()) {
+                        view.model.set('name', view.$name.val() );
+                    }
+            });
+            view.$delete.click(function(){
+                view.model.destroy();
+            });
+
+        },
+        refreshName: function () {
+            this.$name.val( this.model.get('name') );
+        },
+        removeItem: function () {
+            this.remove();
+        }
+    });
+
+    var FriendsListView = Backbone.View.extend({
+        initialize: function () {
+            var view = this;
+            view.viewsList = {};
+            view.collection.map( function(model) {
+                view.addItem(model);
+            } );     // fixme experimental
             view.listenTo(view.collection, 'add', view.addItem);
-            view.listenTo(view.collection, 'reset', view.resetList);
-        },
-        events: {
-            "click": function (event) {
-                $(event.target).toggleClass("selected");
-            },
-            "click [data-class='deleteItem']": function (event) {
-                var target = $(event.target);
-                var cid = target.closest('[data-class="item"]').attr('data-cid');
-                this.collection.remove( this.collection.get(cid) );
-            },
-            "click input": function (event) {
-                event.stopPropagation()
-            },
-            "blur input": function (event) {
-                var target = $(event.target);
-                var cid = target.closest('[data-class="item"]').attr('data-cid');
-                var model = this.collection.get(cid);
-                if (model.get('name') !== target.val()) {
-                    model.set( {name: target.val()} );
-                }
-            }
-        },
-        refreshName: function (model, value) {
-            var selector = '[data-cid=' + model.cid + ']';
-            this.$(selector).find('[data-class="nameInput"]').val(value);
-            console.log(this.collection.map(function(m) {return m.cid}));
-        },
-        removeItem: function (model) {
-            var selector = '[data-cid=' + model.cid + ']';
-            this.$(selector).remove();
-            console.log(this.collection.map(function(m) {return m.cid}));
+            view.listenTo(view.collection, 'destroy remove', view.removeItem);
         },
         addItem: function (model) {
-            var compiled = this.options.template.clone()
-                .attr('data-cid', model.cid)
-                .find('[data-class="nameInput"]')
-                .val(model.get('name'))
-                .end();
-            this.$el.append(compiled);
-            console.log(this.collection.map(function(c) {return c.cid}));
-            console.dir(model);
+            var item = new FriendView({
+                model: model
+            });
+            this.$el.append(item.$el);
+            this.viewsList[model.cid] = item;
         },
-        resetList: function () {
-            this.$el.empty();
-            this.initialize();
+        removeItem: function (model) {
+            this.viewsList[model.cid] = null;
         }
     });
-    var myFriends,
-        myFriendsView;
-    var Controller = {};
-    _.extend(Controller, Backbone.Events);
-    var userId = 'id01';
-    var baseUrl = 'http://localhost:8888/base/';
-    function getFriendsFromServer (data, status) {
-        console.log(status);
-        myFriends = new FriendList(data);
-        myFriendsView = new FriendNamesView({
+    var myFriends = new FriendList(myFriendsFromServer);
+        myFriendsView = new FriendsListView( {
             el: $("#list"),
-            collection: myFriends,
-            template: $('#list [data-class="item"]').detach()
+            collection: myFriends
         });
-        Controller.collection = myFriends;
-        Controller.listenTo(Controller.collection, 'add', Controller.addToServer);
-        Controller.listenTo(Controller.collection, 'remove', Controller.removeFromServer);
-        Controller.listenTo(Controller.collection, 'change', Controller.changeONServer);
-    };
-
-    $.ajax({
-        url: baseUrl + userId  + '/friends',
-        success:  getFriendsFromServer
-    });
 
     $("[data-id='deleteSelected']").click(function () {
-        $("#list .selected").map(function (i, target) {
-            var cid = $(target).attr('data-cid');
-            myFriends.remove( myFriends.get(cid) );
-        })
+        _.values( myFriendsView.viewsList )
+            .filter(function(view) {
+                return view !== null && view.$el.hasClass('selected');
+            })
+            .map(function(view){
+                view.model.destroy()
+            });
     });
     $("[data-id='addItem']").click(function () {
-        myFriends.add(new Friend);
+        myFriends.add({});
     });
-
-    Controller.setModelID = function (data, status, jqXHR) {
-                                                                console.log('responsedata=' + data);
-                                                                console.log(status);
-                                                                console.log(jqXHR);
-        if (typeof data === 'string') {
-            data = JSON.parse(data);
-        }
-        if (typeof data === 'string') {
-            return
-        }
-        myFriends.findWhere({cid: data.cid}).set({id: data.id}, {silent: true});
-        console.dir(myFriends.get(data.id));
-    };
-    Controller.addToServer = function (model) {
-        model.attributes.cid = model.cid;
-        var data = JSON.stringify( model );
-        data = encodeURIComponent(data);
-                                                                   console.log(data);
-        $.ajax({
-            url: baseUrl + userId  + '/friends',
-            type: "POST",
-            data: data,
-            beforeSend: function( jqXHR, settings ) {
-                                                                    console.log('request');
-                                                                        console.dir(jqXHR);
-            }
-        }).done(Controller.setModelID);
-    };
-    Controller.changeResponseHandler = function (data, status, jqXHR) {
-                                                                console.log('responsedata=' + data);
-                                                                console.log(status);
-                                                                console.log(jqXHR);};
-    Controller.changeONServer = function (model) {
-                                                                   console.log(model.id);
-        var data = JSON.stringify( model );
-        data = encodeURIComponent(data);
-                                                                   console.log(data);
-        $.ajax({
-            url: baseUrl + userId  + '/friends/' + model.id,
-            type: "PUT",
-            data: data,
-            beforeSend: function( jqXHR, settings ) {
-                                                                    console.log('request');
-                                                                        console.dir(jqXHR);
-            }
-        }).done(Controller.changeResponseHandler);
-    };
-    Controller.removeResponseHandler = function (data, status, jqXHR) {
-                                                                console.log('responsedata=' + data);
-                                                                console.log(status);
-                                                                console.log(jqXHR);
-    };
-    Controller.removeFromServer = function (model) {
-                                                            console.log(model.id);
-        $.ajax({
-            url: baseUrl + userId  + '/friends/' + model.id,
-            type: "DELETE",
-            beforeSend: function( jqXHR, settings ) {
-                                                                    console.log('request');
-                                                                    console.dir(jqXHR);
-            }
-        }).done(Controller.removeResponseHandler);};
-
-})();
+//})();
